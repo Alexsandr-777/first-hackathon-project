63a47e357f6952335e9157b3dfabbc3748f76883
maxs5 <93215847+maxs5@users.noreply.github.com>
2025-06-29 22:06:02 +0300

    Добавлен background-module/clicks-module/shape-module/spark-module  и стили

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
-import './styles.css'
+import '././styles.css'
 import { CustomMenu } from './menu.js'
 import { DeathScreenModule } from './modules/daeathScreenModule/deathScreenModule.js'
+import { BackgroundModule } from './modules/migrationModule/backgroundModule.js'
+import { ClicksModule } from './modules/migrationModule/clicksModule.js'
+import { ShapeModule } from './modules/migrationModule/shapeModule.js'
+import { SparkModule } from './modules/migrationModule/sparkModule.js'
 const menu = new CustomMenu('#menu')
+
 menu.add(new DeathScreenModule())
+menu.add(new BackgroundModule())
+menu.add(new ClicksModule())
+menu.add(new ShapeModule())
+menu.add(new SparkModule())
+
 
 
