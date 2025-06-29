948077bf13619c0b8c22c8a35eccea68bfda8f81
Alexsandr-777 <sokollex777@gmail.com>
2025-06-29 17:26:33 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
+import { CustomMenu } from './menu.js'
+import { DeathScreenModule } from './modules/daeathScreenModule/deathScreenModule.js'
+const menu = new CustomMenu('#menu')
+menu.add(new DeathScreenModule())
 
