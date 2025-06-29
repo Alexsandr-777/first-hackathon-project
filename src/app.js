58dffdf2ae644fa56b606aed8537471c23a0c714
Alexsandr-777 <sokollex777@gmail.com>
2025-06-29 20:43:24 +0300

    feat: подключить color-change-module в меню

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import './styles.css'
-import { CustomMenu } from './menu.js'
-import { DeathScreenModule } from './modules/daeathScreenModule/deathScreenModule.js'
-const menu = new CustomMenu('#menu')
-menu.add(new DeathScreenModule())
+import './styles.css';
+import { CustomMenu } from './menu.js';
+import { DeathScreenModule } from './modules/daeathScreenModule/deathScreenModule.js';
+import { ColorChangeModule } from './modules/colorChangeModule/colorChangeModule.js';
+const menu = new CustomMenu('#menu');
+menu.add(new DeathScreenModule());
+menu.add(new ColorChangeModule());
 
 
